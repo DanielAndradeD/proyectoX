89168be3ec1fac43231067399391cce54bfb8d4a
DanielAndradeD <danielmetallica98@gmail.com>
2019-05-21 15:58:34 -0500

    BS 4

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hola', function(req, res, next) {
+	//solucitud a una ruta de api rest
+  res.render('saludo', { otraCosa: 'cosa' });
+});
+
 module.exports = router;
